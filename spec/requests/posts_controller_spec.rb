1241b5967cfac47ac7f15a2123652ea1d144287e
jevazquezb <je.vazquezb@gmail.com>
2022-09-27 18:05:56 -0500

    Update request tests to pass the current structure

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
+  before(:each) do
+    @user = User.create(name: 'Javier', photo: 'Photo', bio: "Hello, I'm Javier and I'm a Physicist.")
+    @post = Post.create(title: "Random Post", text: "This is a random post.", author_id: @user.id)
+  end
+
   context '/users/:user_id/posts route (GET #index)' do
-    before(:each) { get '/users/1/posts' }
+    before(:each) { get user_posts_path(@user) }
 
     it 'should be a success when called' do
       expect(response).to have_http_status(:ok)
@@ -13,12 +18,12 @@ RSpec.describe PostsController, type: :request do
     end
 
     it "should render the correct content in the 'index' template" do
-      expect(response.body).to include('Number of posts')
+      expect(response.body).to include('Random Post')
     end
   end
 
   context '/users/:user_id/posts/:id route (GET #show)' do
-    before(:each) { get '/users/1/posts/1' }
+    before(:each) { get user_post_path(@user, @post) }
 
     it 'should be a success when called' do
       expect(response).to have_http_status(:ok)
@@ -29,7 +34,7 @@ RSpec.describe PostsController, type: :request do
     end
 
     it "should render the correct content in the 'show' template" do
-      expect(response.body).to include('Comment 10')
+      expect(response.body).to include('This is a random post.')
     end
   end
 end

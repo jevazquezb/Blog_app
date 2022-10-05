0fd965f95a5f6632c8f5d7982d4a71fdac65b771
jevazquezb <je.vazquezb@gmail.com>
2022-10-05 15:11:13 -0500

    Set up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { confirmations: 'users/confirmations' }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #DSL.- Domain Specific Language
  

52efed0c186b8228b4cdd31a27d3cc8b2be63b11
jevazquezb <je.vazquezb@gmail.com>
2022-10-06 07:55:31 -0500

    Call counter methods after destroy statement for posts and comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,7 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_posts_counter
+  after_destroy :update_posts_counter
 
   def update_posts_counter
     author.update(posts_counter: author.posts.count)

a8fd4c79aa115d0c9461ad7868b2cb7b3d7a77a5
jevazquezb <je.vazquezb@gmail.com>
2022-10-06 18:24:35 -0500

    Add new routes for the API

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,25 @@ Rails.application.routes.draw do
   end
   root "users#index"
 
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :destroy] do
-      resources :comments, only: [:create, :destroy]
-      resources :likes, only: [:create]
-    end
-  end
+  # resources :users, only: [:index, :show] do
+  #   resources :posts, only: [:index, :show, :destroy] do
+  #     resources :comments, only: [:create, :destroy]
+  #     resources :likes, only: [:create]
+  #   end
+  # end
 
   resources :posts, only: [:new, :create]
+
+  # API routes
+  post 'api/v1/auth/login', to: 'authentication#login'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index, :show] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
 end

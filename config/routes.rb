ac485b061cff8c373494b58b23f8dcbad4fe0c61
jevazquezb <je.vazquezb@gmail.com>
2022-10-05 21:16:02 -0500

    Delete confirmations direct access

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { confirmations: 'users/confirmations' }
+  devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end

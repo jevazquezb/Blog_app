e3e4b3370d78372f76c8caf11b097fdbd182d52e
jevazquezb <je.vazquezb@gmail.com>
2022-10-06 18:30:09 -0500

    Remove protect_from_forgery method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :exception
-
+  # protect_from_forgery with: :exception
   before_action :update_allowed_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|

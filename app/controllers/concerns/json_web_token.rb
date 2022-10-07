9fea1b12005035f6632cf584e6273d827a32bd3b
Bushido-brown <orizut@gmail.com>
2022-10-07 09:47:55 -0700

    Edit concerns

--- app/controllers/concerns/json_web_token.rb
+++ app/controllers/concerns/json_web_token.rb
@@ -5,16 +5,13 @@ module JsonWebToken
 
   # The secret must be a string.
   HMAC_SECRET = Rails.application.secrets.secret_key_base
-  # ALGORITHM_TYPE = 'HS256'
 
   def jwt_encode(payload, exp = 3.days.from_now)
     payload[:exp] = exp.to_i
-    # JWT.encode(payload, HMAC_SECRET, ALGORITHM_TYPE)
     JWT.encode(payload, HMAC_SECRET)
   end
 
   def jwt_decode(token)
-    # decoded = JWT.decode(token, HMAC_SECRET, true, { algorithm: ALGORITHM_TYPE })[0]
     decoded = JWT.decode(token, HMAC_SECRET)[0]
     HashWithIndifferentAccess.new decoded
   end

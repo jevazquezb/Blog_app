6a8f51ba98e6cd46623358681c509e252186b538
jevazquezb <je.vazquezb@gmail.com>
2022-10-06 20:38:22 -0500

    Add JsonWebToken module and authenticate_request_method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,17 @@ class ApplicationController < ActionController::API
       u.permit(:name, :photo, :bio, :email, :password, :current_password)
     end
   end
+
+  include JsonWebToken
+
+  before_action :authenticate_request
+
+  private
+
+  def authenticate_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    decoded = jwt_decode(header)
+    @current_user = User.find(decoded[:user_id])
+  end
 end

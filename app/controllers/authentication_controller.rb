f7258eb7265dbb093262a9bc089a013da504896a
jevazquezb <je.vazquezb@gmail.com>
2022-10-06 20:42:26 -0500

    Create authentication controller and add login method

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,14 @@
+class AuthenticationController < ApplicationController
+  skip_before_action :authenticate_request
+  # POST /auth/login
+  def login
+    @user = User.find_by_email(params[:email])
+    # if @user && @user.password == params[:password]
+    if @user.valid_password?(params[:password])
+      token = jwt_encode(user_id: @user.id)
+      render json: { token: token }, status: :ok
+    else
+      render json: { error: 'Invalid credentials' }, status: :unauthorized
+    end
+  end
+end
